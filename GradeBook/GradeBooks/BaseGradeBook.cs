509a9a81a2183ad04079a88e6d2b00f819966dbc
Norbert Lejda <norbertlejda@gmail.com>
2020-11-05 21:08:32 +0100

    Final version

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,15 +9,21 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
-        public string Name { get; set; }
+        public string Name { get; set; }    
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }      
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook()
+        { }
+
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -80,7 +86,7 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Gradebook could not be found.");
                 return null;
-            }
+            }          
 
             using (var file = new FileStream(name + ".gdbk", FileMode.Open, FileAccess.Read))
             {
@@ -106,20 +112,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
             switch (letterGrade)
             {
+                
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;                   
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                gpa++;
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
